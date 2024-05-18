e5e665ff6b06e57f1d1a7eeb5c7673da74d52ba4
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-05-18 15:25:22 +0200

    bg image clean public folder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,36 +4,36 @@
     <div class="flex">
         <img src="{{asset('images/socialmd.png')}}" alt="Phone" width="25%" class=""/>
         <div>
-            <img src="{{ asset('images/WebLogo.png') }}" alt="Students" class="w-96 h-20" />
-            <p class="mt-10 text-white">Soon to be home of Frontpage Social Platform </p>
-            <p class="mt-5 text-white">frontpage.social <span class="text-maroon">by MSHDLIVE</span></p>
-            <div class="flex space-x-2 mt-5">
-                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32"> <!-- First div -->
+            <img src="{{ asset('images/WebLogo.png') }}" alt="Students" class="w-full h-32" />
+            <p class="mt-2 text-white text-center">Soon to be home of Frontpage Social Platform </p>
+            <p class="mt-5 text-white text-center">frontpage.social <span class="text-maroon">by MSHDLIVE</span></p>
+            <div class="flex space-x-2 mt-5 justify-center mx-auto w-8/12">
+                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32 flex flex-col justify-center items-center"> <!-- First div -->
                     <h1 class="text-2xl">188</h1>
                     <p class="text-xs font-thin">DAYS</p>
                     <div class="absolute bottom-0 left-0 right-0 bg-white h-2"></div> <!-- White section at the bottom -->
                 </div>
 
-                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32"> <!-- Second div -->
+                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32 flex flex-col justify-center items-center"> <!-- Second div -->
                     <h1 class="text-2xl">12</h1>
                     <p class="text-xs font-thin">HOURS</p>
                     <div class="absolute bottom-0 left-0 right-0 bg-white h-2"></div> <!-- White section at the bottom -->
                 </div>
 
-                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32"> <!-- Third div -->
+                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32 flex flex-col justify-center items-center"> <!-- Third div -->
                     <h1 class="text-2xl">19</h1>
                     <p class="text-xs font-thin">MINUTES</p>
                     <div class="absolute bottom-0 left-0 right-0 bg-white h-2"></div> <!-- White section at the bottom -->
                 </div>
 
-                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32"> <!-- Fourth div -->
+                <div class="bg-maroon text-white py-3 px-4 text-center relative w-28 h-32 flex flex-col justify-center items-center"> <!-- Fourth div -->
                     <h1 class="text-2xl">22</h1>
                     <p class="text-xs font-thin">SECONDS</p>
                     <div class="absolute bottom-0 left-0 right-0 bg-white h-2"></div> <!-- White section at the bottom -->
                 </div>
             </div>
-            <p class="mt-10 text-white">Notify me when ready</p>
-            <div class="justify-center mt-4 flex">
+            <p class="mt-10 text-white text-center">Notify me when ready</p>
+            <div class="mt-4 flex w-8/12 justify-center mx-auto">
                 <input
                     id="email"
                     type="email"
@@ -48,6 +48,25 @@
                     Send
                 </button>
             </div>
+            <div class="mt-10">
+                <div class="flex justify-center space-x-12 mt-4">
+                    <a href="#" class="text-black bg-white hover:text-maroon px-4 hover:bg-gray-200 rounded-full p-2">
+                        <i class="fab fa-facebook-f text-3xl"></i>
+                    </a>
+                    <a href="#" class="text-black bg-white hover:text-maroon px-3 hover:bg-gray-200 rounded-full p-2">
+                        <i class="fab fa-twitter text-3xl"></i>
+                    </a>
+                    <a href="#" class="text-black bg-white hover:text-maroon px-3  hover:bg-gray-200 rounded-full p-2">
+                        <i class="fab fa-instagram text-3xl"></i>
+                    </a>
+                    <a href="#" class="text-black bg-white hover:text-maroon px-3  hover:bg-gray-200 rounded-full p-2">
+                        <i class="fab fa-linkedin-in text-3xl"></i>
+                    </a>
+                </div>
+            </div>
+            </div>
         </div>
+
     </div>
+
 </div>
